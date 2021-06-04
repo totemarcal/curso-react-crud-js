9db73847cf86f6ac12fa430d1f9c5be39b52769b
DuanCamargo <duan1505@gmail.com>
2021-06-04 16:01:29 -0300

    update_20210604

--- src/components/AddTutorial.js
+++ src/components/AddTutorial.js
@@ -1,12 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import TutorialDataService from "../services/TutorialDataService";
+import Tutorial from "./Tutorial";
 
 const AddTutorial = () => {
-  
+  const initialTutorialState = {
+    id: null,
+    title: "",
+    description: "",
+    published: false
+  };
+  const [tutorial, setTutorial] = useState(initialTutorialState);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setTutorial({ ...tutorial, [name]: value });
+  };
+
+  const saveTutorial = () => {
+    TutorialDataService.create(tutorial);
+    setSubmitted(true);
+  };
+
+  const newTutorial = () => {
+    setTutorial(initialTutorialState);
+    setSubmitted(false);
+  };
+
   return (
-    <div >
-      
+    <div className="submit-form">
+      {submitted ? (
+        <div>
+          <h4>You submitted successfully!</h4>
+          <button className="btn btn-success" onClick={newTutorial}>
+            Add
+          </button>
+        </div>
+      ) : (
+        <div>
+          <form onSubmit={saveTutorial}>
+            <div className="form-group">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                className="form-control"
+                id="title"
+                required
+                value={tutorial.title}
+                onChange={handleInputChange}
+                name="title"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                className="form-control"
+                id="description"
+                required
+                value={tutorial.description}
+                onChange={handleInputChange}
+                name="description"
+              />
+            </div>
+            <button type="submit" className="btn btn-success">
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
-
 export default AddTutorial;
--- src/components/TutorialsList.js
+++ src/components/TutorialsList.js
@@ -1,10 +1,86 @@
-import React from "react";
+import React, {useState}from "react";
+import { Link } from "react-router-dom";
+import TutorialDataService from "../services/TutorialDataService";
 
 const TutorialList = () => {
-  
+  const [searchTitle, setSearchTitle] = useState("")
+  const [tutorials, setTutorials] = useState(TutorialDataService.getAll());
+
+  const onChangeSearchTitle = e => {
+    const searchTitle = e.target.value;
+    setSearchTitle(searchTitle);
+  };
+
+  const findByTitle = () => {
+    setTutorials(TutorialDataService.getById(searchTitle))
+    // alert("Em Construção");
+  };
+
+  const deleteTutorial = (title) => {
+    alert("Title"+title)
+  };
+
+  const removeAllTutorials = () => {
+
+  };
+
   return (
-    <div >
-      
+    <div className="list row">
+      <div className="col-md-10">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search by title"
+            value={searchTitle}
+            onChange={onChangeSearchTitle}
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={findByTitle}>
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-10">
+        <h4>Tutorials List</h4>
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Title</th>
+              <th scope="col">Description</th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+          { 
+            tutorials &&
+            tutorials.map((tutorial, index) => (
+              <tr>
+                <th scope="row">{tutorial.id}</th>
+                <td>{tutorial.title}</td>
+                <td>{tutorial.description}</td>
+                <td> <Link to={"/tutorials/" + tutorial.title}
+                  className="badge badge-warning">Edit</Link>
+                </td>
+                <td> <Link onClick={() => deleteTutorial(tutorial.title)}
+                  className="badge badge-danger">Remove</Link>
+                </td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
+        <button
+          className="m-3 btn btn-sm btn-danger"
+          onClick={removeAllTutorials}>
+          Remove All
+        </button>
+      </div>
     </div>
   );
 };
